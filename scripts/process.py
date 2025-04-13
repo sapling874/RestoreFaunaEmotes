2c5527f0095c26b398b1bc58b0d93d49d1c0137b
sapling874 <sapling874@gmail.com>
2025-04-13 17:14:48 +0100

    Parse superchats and member messages from raw data

--- scripts/process.py
+++ scripts/process.py
@@ -20,40 +20,56 @@ out_filename = pathlib.Path("chat_data") / filename.name
 
 chat_data = {}
 
+def parse(renderer):
+    timestamp = renderer["timestampUsec"]
+    message = renderer["message"]
+
+    emotes = []
+    if any("emoji" in run for run in message["runs"]):
+        for run in message["runs"]:
+            if "emoji" not in run:
+                continue
+            if not run["emoji"].get("isCustomEmoji", False):
+                continue
+            if len(run["emoji"]["shortcuts"]) == 1:
+                # This differentiates Fauna custom emotes
+                # from generic youtube custom emotes.
+                # Channel emotes have two shortcuts, e.g
+                # ":love:" and ":_love", youtube emotes
+                # only have one.
+                continue
+            emoji = run["emoji"]
+            name = emoji["shortcuts"][1].replace(":", "")
+            emotes.append(name)
+
+    return timestamp, emotes
+
 with open(filename) as f:
     for line in f.readlines():
         chat_message = json.loads(line)
 
-        if "addChatItemAction" not in chat_message["replayChatItemAction"]["actions"][0]:
+        action = chat_message["replayChatItemAction"]["actions"][0]
+        if not "addChatItemAction" in action:
             continue
-        chat_action = chat_message["replayChatItemAction"]["actions"][0]["addChatItemAction"]
-        if "liveChatTextMessageRenderer" not in chat_action["item"]:
+        item = action["addChatItemAction"]["item"]
+        if "liveChatTextMessageRenderer" in item:
+            # Parse a normal chat message.
+            message_renderer = item["liveChatTextMessageRenderer"]
+            timestamp, emotes = parse(message_renderer)
+        elif "liveChatMembershipItemRenderer" in item:
+            # Membership message.
+            message_renderer = action["addChatItemAction"]["item"]["liveChatMembershipItemRenderer"]
+            timestamp, emotes = parse(message_renderer)
+        elif "liveChatPaidMessageRenderer" in item:
+            # Superchat message.
+            message_renderer = item["liveChatPaidMessageRenderer"]
+            timestamp, emotes = parse(message_renderer)
+        else:
             continue
 
-        message = chat_action["item"]["liveChatTextMessageRenderer"]["message"]
-        timestamp = chat_action["item"]["liveChatTextMessageRenderer"]["timestampUsec"]
-
-        emojis = []
-        if any("emoji" in run for run in message["runs"]):
-            for run in message["runs"]:
-                if "emoji" not in run:
-                    continue
-                if not run["emoji"].get("isCustomEmoji", False):
-                    continue
-                if len(run["emoji"]["shortcuts"]) == 1:
-                    # This differentiates Fauna custom emotes
-                    # from generic youtube custom emotes.
-                    # Channel emotes have two shortcuts, e.g
-                    # ":love:" and ":_love", youtube emotes
-                    # only have one.
-                    continue
-                emoji = run["emoji"]
-                name = emoji["shortcuts"][1].replace(":", "")
-                emojis.append(name)
-
-            if len(emojis) > 0:
-                assert timestamp not in chat_data
-                chat_data[timestamp] = encode.encode_emotes(emojis)
+        if len(emotes) > 0:
+            assert timestamp not in chat_data
+            chat_data[timestamp] = encode.encode_emotes(emotes)
 
 print("Output file: ", out_filename)
 print("Total messages stored: ", len(chat_data))
