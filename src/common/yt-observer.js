74ada223649c748efd8c291bc18a92204b5f9ecc
sapling874 <sapling874@gmail.com>
2025-04-30 00:40:15 +0100

    Fix for badges being replaced multiple times
    To reproduce
    * start a stream
    * let a few seconds play
    * scrub back to the 0:00
    
    The messages would be shuffled around
    in the chat box and passed through the
    observer a second time.
    
    The badges would then have their "data" URL
    passed into the function to get the badge ID,
    and it would return undefined resulting in
    a broken image icon being displayed.

--- src/common/yt-observer.js
+++ src/common/yt-observer.js
@@ -9,6 +9,12 @@ function replaceBadge(node) {
 	}
 	badge = node.querySelector("yt-live-chat-author-chip > #chat-badges > yt-live-chat-author-badge-renderer > #image > img");
 	if (badge) {
+		if (badge.src.startsWith("data:img/png")) {
+			// Reparsing an already replaced badge.
+			// Happens when quickly scrubbing back to the start of a stream.
+			// Don't need to do anything.
+			return;
+		}
 		badgeId = getBadgeIdFromUrl(badge.src);
 		if (badgeId) {
 			badge.src = badgesData[badgeId];
